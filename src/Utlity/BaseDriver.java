227d308e40c3e8f5ee16e3ab1ef7ae5d34ba5f9e
AyGulll <abilovaygul@gmail@com>
2024-04-18 15:02:59 +0100

    Utlity

---
+++ src/Utlity/BaseDriver.java
@@ -0,0 +1,23 @@
+package Utlity;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class BaseDriver {
+    public static WebDriver driver;
+    public static WebDriver wait;
+    static {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        wait = (WebDriver) new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+    public static void BekleVeKapat(){
+        MyFunc.Bekle(5);
+        driver.quit();
+    }
+
+}
+
+
---
+++ src/Utlity/MyFunc.java
@@ -0,0 +1,16 @@
+package Utlity;
+
+public class MyFunc {
+
+    public static void Bekle(int sn){
+
+        try {
+            Thread.sleep(sn*1000l);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+}
