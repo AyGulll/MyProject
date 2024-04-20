c3aa23a2c9d65b698fe54ca5b36c38f3418c9a77
AyGulll <abilovaygul@gmail@com>
2024-04-20 21:05:29 +0100

    Login loading

--- src/Rossmann/Login.java
+++ src/Rossmann/Login.java
@@ -2,8 +2,13 @@ package Rossmann;
 
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {
      public static WebDriver driver;
        @Test(dataProvider="UserData", groups={"Smoke","Login"});
+       public void login(String username, String password){
+           driver=new ChromeDriver();
+           driver.get("https://www.rossmann.pl/");
+       }
 }
