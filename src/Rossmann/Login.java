2c911e7ab1698723df846564710756ce39e306d1
AyGulll <abilovaygul@gmail@com>
2024-04-21 21:42:37 +0100

    Login loading 2

--- src/Rossmann/Login.java
+++ src/Rossmann/Login.java
@@ -1,7 +1,10 @@
 package Rossmann;
 
+import Utlity.MyFunc;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {
@@ -10,5 +13,8 @@ public class Login {
        public void login(String username, String password){
            driver=new ChromeDriver();
            driver.get("https://www.rossmann.pl/");
+           driver.manage().window().maximize();
+           MyFunc.Bekle(2);
+           WebElement profil=driver.findElement(By.xpath(""));
        }
 }
