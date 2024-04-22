1f71accb62f2d0b2a009fe6cc97109914e761e61
AyGulll <abilovaygul@gmail@com>
2024-04-22 21:26:54 +0100

    Login loading 3

--- src/Rossmann/Login.java
+++ src/Rossmann/Login.java
@@ -9,12 +9,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {
      public static WebDriver driver;
-       @Test(dataProvider="UserData", groups={"Smoke","Login"});
+       @Test(dataProvider="UserData", groups={"Smoke","Login"})
        public void login(String username, String password){
            driver=new ChromeDriver();
            driver.get("https://www.rossmann.pl/");
            driver.manage().window().maximize();
            MyFunc.Bekle(2);
-           WebElement profil=driver.findElement(By.xpath(""));
+           WebElement profil=driver.findElement(By.xpath("//span[normalize-space()='Profil']"));
+           profil.click();
+           WebElement login=driver.findElement(By.xpath("//font[contains(text(),'Hesab yarat')]"));
+           login.click();
        }
+
 }
